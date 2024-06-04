edaf1cd080d4a7dc495e1df4fd52428809d3ea2e
DavidPowerBi <159034323+DavidPowerBi@users.noreply.github.com>
2024-06-04 19:58:03 +0000

    1° Tentativa de deploy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require("./config/config");
 //const bodyParser = require("body-parser");
 //const exphbs = require("express-handlebars");
 const sequelize = require("./config/database");
